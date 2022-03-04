77acd85fbd7c0999b1918f6f4dee1ab5ee025d24
Mathias <31206630+Mattertorian@users.noreply.github.com>
2022-03-04 20:22:49 +0100

    update config.js with more default fields for datepicker

--- packages/oruga/src/utils/config.js
+++ packages/oruga/src/utils/config.js
@@ -3,7 +3,28 @@ import { merge } from "./helpers"
 let config = {
     iconPack: 'mdi',
     useHtml5Validation: true,
-    statusIcon: true
+    statusIcon: true,
+    defaultIconComponent: 'fa',
+    defaultIconPack: 'fas',
+    defaultLocale: 'da-DK',
+    defaultDayNames: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
+    defaultTooltipDelay: 400,
+    defaultMonthNames: [
+          'Januar',
+          'Februar',
+          'Marts',
+          'April',
+          'Maj',
+          'Juni',
+          'Juli',
+          'August',
+          'September',
+          'Oktober',
+          'November',
+          'December'
+        ],
+    defaultFirstDayOfWeek: 1,
+    defaultModalScroll: 'keep'
 }
 
 export const setOptions = (options) => { config = options }
